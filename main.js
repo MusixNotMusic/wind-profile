5add7966e24b98497ad56c0596295fc067281261
musixnotmusic <279601306@qq.com>
2022-07-18 14:55:02 +0800

    ...

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
 import { createZoomDemo } from './src/d3-demo'
+import { WindProfileSvg } from './src/wind-profile-svg'
 
-const svg = createZoomDemo({ width: 1000, height: 600 });
-
-document.querySelector('#app').append(svg);
+// const svg = createZoomDemo({ width: 1000, height: 600 });
+// document.querySelector('#app').append(svg);
+const windProfileSvg = new WindProfileSvg({ width: 1000, height: 600, top: 20, left: 20, bottom: 40, right: 30});
+document.querySelector('#app').append(windProfileSvg.svgDom);
